102861639fda467789bf6ca17c2335c775e2670e
Matej <matejchalachan@gmail.com>
2020-06-15 19:34:35 +0200

    controls moved to own component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,7 @@
   <div id="app" class="container">
     <ScoreBoard :score=0 />
 
-    <div class="btns-container">
-      <div>
-        <button class="btn" @click="reset">Reset game</button>
-        <button class="btn" @click="shuffle">Shuffle</button>
-      </div>
-
-      <div>
-        <span class="info">How many different cards do you want: </span> <input type="number" min="1" max="50" value="20" ref="cardCountInput" class="input">
-        <button class="btn" @click="pickNewSet">Pick</button>
-      </div>
-    </div>
+    <ControlBoard  @reseted="reset" @shuffled="shuffle" @picked="pickNewSet"/>
 
     <div class="cards-container">
       <Card v-for="card in cards" :key="card.id" :cardData="card" @flipped="flipCard"/>
@@ -25,12 +15,14 @@ import ScoreBoard from './components/ScoreBoard.vue'
 import Card from './components/Card'
 import cardsArray from './cards'
 import Vue from 'vue'
+import ControlBoard from "@/components/ControlBoard";
 
 export default {
   name: 'App',
   components: {
     ScoreBoard,
-    Card
+    Card,
+    ControlBoard
   },
   mounted: function() {
 
@@ -72,10 +64,10 @@ export default {
       this.shuffleCards();
 
     },
-    pickNewSet: function() {
+    pickNewSet: function(value) {
       this.cards = [];
 
-      let cards = this.getRandomCards(this.$refs.cardCountInput.value);
+      let cards = this.getRandomCards(value);
 
       this.generateCardsPairs(cards);
 
@@ -161,68 +153,6 @@ export default {
     margin-left: 15px;
     margin-right: 15px;
     margin-top: 15px;
-  }
-
-  .btns-container {
-    padding: 15px 30px;
-    display: flex;
-    justify-content: space-between;
-    flex-direction: row;
-    flex-wrap: wrap;
-    border-bottom: 2px solid gray;
-    background-color: rgba(255,255,255,0.7);
-    margin-bottom: 30px;
-
-    box-shadow:
-            0 0 10px #fff,            /* outer white */
-            -10px 0 25px #7a007a,        /* outer left magenta */
-            10px 10px 25px #006b6b;         /* outer right cyan */
-  }
-
-  .info {
-    color: black;
-    font-size: 1.2em;
-    font-weight: bold;
-  }
-
-  .btn {
-    backface-visibility: hidden;
-    background-color: #0a0a0a;
-    border: 2px solid black;
-    border-radius: 4px;
-    color: rgb(255, 255, 255);
-    cursor: pointer;
-    display: inline-block;
-    font-weight: bold;
-    letter-spacing: 0.1em;
-    padding: 1em 2em;
-    text-align: center;
-    text-decoration: none;
-    text-transform: uppercase;
-    transition: all 0.3s ease 0s;
-    margin-left: 15px;
-    margin-right: 15px;
-  }
-
-  .btn:hover {
-    box-shadow: rgba(0, 0, 0, 0.1) 0 0.3rem 1rem;
-    background-color: rgba(10, 10, 10, 0.8);
-  }
-
-  .input {
-    padding: 1em 2em;
-    border-radius: 4px;
-    border: 2px solid black;
-  }
-
-  .input[type=number]::-webkit-inner-spin-button,
-  .input[type=number]::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-
-  .input[type=number] {
-    -moz-appearance: textfield;
   }
 
 </style>
---
+++ src/components/ControlBoard.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="btns-container">
+        <div>
+            <button class="btn" @click="reset">Reset game</button>
+            <button class="btn" @click="shuffle">Shuffle</button>
+        </div>
+
+        <div>
+            <span class="info">How many different cards do you want: </span> <input type="number" min="1" max="50" value="20" ref="cardCountInput" class="input">
+            <button class="btn" @click="pickNewSet">Pick</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ControlBoard",
+        methods: {
+            reset: function () {
+                this.$emit('reseted');
+            },
+            shuffle: function () {
+                this.$emit('shuffled');
+            },
+            pickNewSet: function () {
+                this.$emit('picked',this.$refs.cardCountInput.value);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .btns-container {
+        padding: 15px 30px;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: row;
+        flex-wrap: wrap;
+        border-bottom: 2px solid gray;
+        background-color: rgba(255,255,255,0.7);
+        margin-bottom: 30px;
+
+        box-shadow:
+                0 0 10px #fff,            /* outer white */
+                -10px 0 25px #7a007a,        /* outer left magenta */
+                10px 10px 25px #006b6b;         /* outer right cyan */
+    }
+
+    .info {
+        color: black;
+        font-size: 1.2em;
+        font-weight: bold;
+    }
+
+    .btn {
+        backface-visibility: hidden;
+        background-color: #0a0a0a;
+        border: 2px solid black;
+        border-radius: 4px;
+        color: rgb(255, 255, 255);
+        cursor: pointer;
+        display: inline-block;
+        font-weight: bold;
+        letter-spacing: 0.1em;
+        padding: 1em 2em;
+        text-align: center;
+        text-decoration: none;
+        text-transform: uppercase;
+        transition: all 0.3s ease 0s;
+        margin-left: 15px;
+        margin-right: 15px;
+    }
+
+    .btn:hover {
+        box-shadow: rgba(0, 0, 0, 0.1) 0 0.3rem 1rem;
+        background-color: rgba(10, 10, 10, 0.8);
+    }
+
+    .input {
+        padding: 1em 2em;
+        border-radius: 4px;
+        border: 2px solid black;
+    }
+
+    .input[type=number]::-webkit-inner-spin-button,
+    .input[type=number]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    .input[type=number] {
+        -moz-appearance: textfield;
+    }
+</style>
